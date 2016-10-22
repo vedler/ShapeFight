f48956efa54b00176c1afe77165d40f1d36b2621
Andrus Lall <andruslall@outlook.com>
2016-10-23 01:37:24 +0300

    no message

---
+++ Assets/TestPool/TestManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+    public GameObject prefab;
+
+    void Start()
+    {
+        WeaponManager.instance.CreatePool(prefab, 3);
+    }
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            WeaponManager.instance.ReuseObject(prefab, Vector3.zero, Quaternion.identity);
+        }
+	}
+}
