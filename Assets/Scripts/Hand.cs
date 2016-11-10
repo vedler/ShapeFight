1e6d7d451a12ce8a2d3e311040162bf61f333f14
Yootah <juta.mae13@gmail.com>
2016-11-10 20:53:31 +0200

    Added the hand script and my own Testing Scene, ref #2

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        playerTransform = gameObject.GetComponentInParent<Transform>();
+    }
+
+   // Update is called once per frame
+   void Update () {
+
+      /* Uncomment if we decide to make the Hand a stand-alone object
+       * 
+       * transform.position = playerTransform.position 
+       */
+
+        // Get target rotation point from mouse, pivot point from self
+        Vector2 targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition), 
+                myPivot = new Vector3(transform.position.x, transform.position.y);
+
+        // Calculate the right direction
+        Vector2 direction = targetPoint - myPivot;
+        direction.Normalize();
+
+        // QUATERNION-IFY IT!
+        Quaternion rotateTo = Quaternion.Euler(0, 0, (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + 180);
+
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotateTo, 5f * Time.deltaTime);
+             
+    }
+}
