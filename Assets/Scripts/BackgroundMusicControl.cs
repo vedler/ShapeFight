92aeff16f9aae230ace1c478a374c4df00d0b008
Andrus Lall <andruslall@outlook.com>
2016-12-15 18:01:40 +0200

    Sound shit

---
+++ Assets/Scripts/BackgroundMusicControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class BackgroundMusicControl : MonoBehaviour {
+
+
+    public AudioMixerSnapshot defaultSnap;
+    public AudioMixerSnapshot beginGame;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("ENTERED SOUNDTRACK TRIGGER");
+        if (other.CompareTag("LocalPlayerTag"))
+        {
+            beginGame.TransitionTo(0.1f);
+        }
+    }
+}
