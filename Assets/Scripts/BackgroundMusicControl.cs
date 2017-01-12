97f21fdb72e2e129bc1c2184f92be545fcf11f08
Andrus Lall <andruslall@outlook.com>
2017-01-12 10:50:36 +0200

    Added another soundtrack and tweaked audiomixer settings

--- Assets/Scripts/BackgroundMusicControl.cs
+++ Assets/Scripts/BackgroundMusicControl.cs
@@ -8,6 +8,7 @@ public class BackgroundMusicControl : MonoBehaviour {
     public AudioMixerSnapshot defaultSnap;
     public AudioMixerSnapshot beginGame;
     public AudioMixerSnapshot beginGame2;
+    public AudioMixerSnapshot beginGame3;
     private bool started = false;
 
 
@@ -22,15 +23,20 @@ public class BackgroundMusicControl : MonoBehaviour {
         print("ENTERED SOUNDTRACK TRIGGER");
         if (!started && other.CompareTag("LocalPlayerTag"))
         {   
-            float a = Random.Range(0.0f, 1.0f);
-            if (a < 0.5f)
+            float a = Random.Range(0.0f, 1.2f);
+            if (a < 0.4f)
             {
                 backgroundMusic.GetComponents<AudioSource>()[0].Play();
                 beginGame.TransitionTo(0.01f);
             }
-            else {
+            else if (a >= 0.4f && a < 0.8f)
+            {
                 backgroundMusic.GetComponents<AudioSource>()[1].Play();
                 beginGame2.TransitionTo(0.01f);
+            }
+            else {
+                backgroundMusic.GetComponents<AudioSource>()[2].Play();
+                beginGame3.TransitionTo(0.01f);
             }
             started = true;
             GetComponent<Collider2D>().enabled = false;
